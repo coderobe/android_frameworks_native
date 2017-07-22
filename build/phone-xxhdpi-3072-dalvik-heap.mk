55ce1a3618ef60800450c0d2eefcbd6453f63861
Robin B <robin@broda.me>
2017-07-22 03:16:25 +0200

    Add overrides to configure the Dalvik heap for a 3G phone

---
+++ build/phone-xxhdpi-3072-dalvik-heap.mk
@@ -0,0 +1,9 @@
+# Provides overrides to configure the Dalvik heap for a 3G phone
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
